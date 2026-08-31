cfcf66dc8e0809f4f5568e93ce75a7e5cc8ce57b
Ashna-Virdikar <65767889+AshnaVirdikar@users.noreply.github.com>
2026-08-31 13:02:18 +0530

    Add files via upload

---
+++ Payment funnel analysis.sql
@@ -0,0 +1,24 @@
+WITH max_status_reached as (SELECT  SubscriptionID, 
+								    MAX(StatusID) as maxstatus
+									FROM paymentstatuslog 
+									Group By SubscriptionID
+                                                ),
+paymentfunnelstage as (SELECT SubscriptionID,
+                                           case when maxstatus = 1 then 'PaymentWidgetOpened'
+                                                     when maxstatus = 2 then 'PaymentEntered'
+        when maxstatus = 3 and currentstatus = 0 then 'User Error with Payment Submission'
+        when maxstatus = 3 and currentstatus != 0 then 'Payment Submitted'
+        when maxstatus = 4 and currentstatus = 0 then 'Payment Processing Error with Vendor'
+        when maxstatus = 4 and currentstatus != 0 then 'Payment Success'
+        when maxstatus = 5 then 'Complete'
+        when maxstatus is null then 'User did not start payment process'
+        end as paymentfunnelstage
+FROM Subscriptions subs
+LEFT JOIN   Max_status_reached m
+on subs.SubscriptionID = m.SubscriptionID
+)
+SELECT paymentfunnelstage, 
+COUNT(SubscriptionID)
+FROM paymentfunnelstage
+GROUP By paymentfunnelstage 
+;
